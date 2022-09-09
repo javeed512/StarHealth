6a3e6d47beb45a704c2b65332ba74b3945a9f9f7
javeed512 <50737382+javeed512@users.noreply.github.com>
2022-09-09 13:44:20 +0530

    Java Intro Day1 Notes

---
+++ firstapp/src/mypackage/Demo.java
@@ -0,0 +1,25 @@
+package mypackage;
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+			int x = 33;
+			int rollNo =99;
+			String name = "Javeed";
+		
+				byte b = 99;
+				short s = 100;
+				
+				b =(byte) s;
+			
+		
+			System.out.println("Hello World!");
+			System.out.println("Javeed");
+			
+				x =99;
+			System.out.println(x);
+
+	}
+
+}
